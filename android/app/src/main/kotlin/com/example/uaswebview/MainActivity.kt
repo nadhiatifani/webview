7ab4e05a341b34404f0bba32eab42f151432863d
Nadia <nadhiatifani412@gmail.com>
2023-01-17 18:07:54 +0700

    second commit

---
+++ android/app/src/main/kotlin/com/example/uaswebview/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.uaswebview
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:uaswebview/homepage.dart';
+import 'package:uaswebview/homepage.dart';
+import 'package:uaswebview/webview.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    ),
+  );
+}
